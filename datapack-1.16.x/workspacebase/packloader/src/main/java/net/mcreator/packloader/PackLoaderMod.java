2fe87fa8db9292e18fcdd8fcd05a608a796f21ff
Spectrall368 <105946758+Spectrall368@users.noreply.github.com>
2025-08-18 16:57:44 +0200

    Fixes #13

--- datapack-1.16.x/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
+++ datapack-1.16.x/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
@@ -1,24 +1,29 @@
 package net.mcreator.packloader;
 
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.FolderPack;
+import net.minecraft.resources.IPackNameDecorator;
+import net.minecraft.resources.ResourcePackInfo;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.AddPackFindersEvent;
-
-import net.minecraft.server.packs.repository.PackSource;
-import net.minecraft.server.packs.repository.FolderRepositorySource;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.io.File;
 
 @Mod(PackLoaderMod.MODID) public class PackLoaderMod {
 
-	public static final String MODID = "packloader";
+    public static final String MODID = "packloader";
 
-	public PackLoaderMod() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addPacks);
-	}
+    public PackLoaderMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::addPacks);
+    }
 
-	public void addPacks(AddPackFindersEvent event) {
-		event.addRepositorySource(new FolderRepositorySource(new File("datapacks"), PackSource.DEFAULT));
-	}
-
+    public void addPacks(FMLClientSetupEvent event) {
+        Minecraft mc = event.getMinecraftSupplier().get();
+        mc.getResourcePackRepository().addPackFinder((consumer, iFactory) -> {
+            if (mc.player != null)
+                consumer.accept(ResourcePackInfo.create("packloader", true, () -> new FolderPack(new File("datapacks")), iFactory, ResourcePackInfo.Priority.TOP,
+                        IPackNameDecorator.BUILT_IN));
+        });
+    }
 }
--- resourcepack-1.16.5/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
+++ resourcepack-1.16.5/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
@@ -1,7 +1,6 @@
 package net.mcreator.packloader;
 
 import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,47 +10,47 @@ import net.minecraft.client.Minecraft;
 
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Collection;
 import java.util.ArrayList;
-
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.io.File;
 
 @Mod(PackLoaderMod.MODID) public class PackLoaderMod {
 
-	public static final String MODID = "packloader";
+    public static final String MODID = "packloader";
 
-	@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-	public static class ClientModEvents {
-		@SubscribeEvent public static void onClientSetup(FMLClientSetupEvent event) {
-			List<String> resourcePacks = new ArrayList<>();
-			var resourcePacksPath = FMLPaths.getOrCreateGameRelativePath(Path.of("resourcepacks"), "resourcepacks");
-			if (resourcePacksPath.toFile().exists()) {
-				File[] files = resourcePacksPath.toFile().listFiles();
-				if (files != null) {
-					for (var file : files) {
-						resourcePacks.add(file.getName());
-					}
-				}
-			}
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent public static void onClientSetup(FMLClientSetupEvent event) {
+            List<String> resourcePacks = new ArrayList<>();
+            Path resourcePacksPath = FMLPaths.getOrCreateGameRelativePath(Paths.get("resourcepacks"), "resourcepacks");
+            if (resourcePacksPath.toFile().exists()) {
+                File[] files = resourcePacksPath.toFile().listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        resourcePacks.add(file.getName());
+                    }
+                }
+            }
 
-			boolean anyChanged = false;
-			List<String> selectedPacks = new LinkedList<>(Minecraft.getInstance().getResourcePackRepository().getSelectedIds());
-			var allPacks = Minecraft.getInstance().getResourcePackRepository().getAvailableIds();
-			for (String pack : allPacks) {
-				for (String resourcePack : resourcePacks) {
-					if (pack.contains(resourcePack)) {
-						anyChanged = true;
-						selectedPacks.add(pack);
-					}
-				}
-			}
+            boolean anyChanged = false;
+            List<String> selectedPacks = new LinkedList<>(Minecraft.getInstance().getResourcePackRepository().getSelectedIds());
+            Collection<String> allPacks = Minecraft.getInstance().getResourcePackRepository().getAvailableIds();
+            for (String pack : allPacks) {
+                for (String resourcePack : resourcePacks) {
+                    if (pack.contains(resourcePack)) {
+                        anyChanged = true;
+                        selectedPacks.add(pack);
+                    }
+                }
+            }
 
-			if (anyChanged) {
-				Minecraft.getInstance().getResourcePackRepository().setSelected(selectedPacks);
-				Minecraft.getInstance().options.loadSelectedResourcePacks(Minecraft.getInstance().getResourcePackRepository());
-			}
-		}
-	}
-
+            if (anyChanged) {
+                Minecraft.getInstance().getResourcePackRepository().setSelected(selectedPacks);
+                Minecraft.getInstance().options.loadSelectedResourcePacks(Minecraft.getInstance().getResourcePackRepository());
+            }
+        }
+    }
 }
